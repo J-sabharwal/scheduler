0eb220022e99c1d7653fca5d701554829c983fb6
Jaspinder Sabharwal <jas@jaspinder.net>
2020-04-15 15:14:18 -0700

    added appointments to main interface

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,54 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Jason Daniels",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Penny Wright",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
+];
 
 const days = [
   {
@@ -24,8 +72,20 @@ const days = [
 
 
 export default function Application(props) {
-
+  
   const [currentDay, setDay] = useState("Monday")
+  
+  const appointmentSlot =  appointments.map((timeslot, index) => {
+    console.log(timeslot)
+    return (
+    <Appointment 
+      key={timeslot.id} 
+      {...timeslot }
+    />
+    
+    );
+  });
+
 
   return (
     <main className="layout">
@@ -50,7 +110,8 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentSlot}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 export default function Appointment(props) {
 
+  const { id, interview, time, onEdit, onDelete} = props
+
+
   return (
-    <article className="Appointment"></article>
+    <article className="Appointment">
+      <Header time={time} />
+      {interview ? 
+        <Show {...interview}/> : 
+        <Empty />
+        
+      }
+        
+    </article>
 
   )
 }
