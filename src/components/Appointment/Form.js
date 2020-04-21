2d281cfc25d9a2f1fd85831794ebbd6107136838
Jaspinder Sabharwal <jas@jaspinder.net>
2020-04-20 22:57:43 -0700

    added jest testing to form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,18 +4,29 @@ import InterviewerList from "components/InterviewerList"
 
 export default function Form(props) {
 
+  const [error, setError] = useState("");
+
   const { onSave, onCancel, interviewers, creating  } = props;
 
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer, creating);
+  }
+  
   function inputName(event) {
     setName(event.target.value)
   }
 
-  function handleInterviewer(event) {
-    setInterviewer(event)
-  }
+  // function handleInterviewer(event) {
+  //   setInterviewer(event)
+  // }
 
   function reset() {
     setName("");
@@ -27,6 +38,8 @@ export default function Form(props) {
     onCancel();
   }
 
+
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -37,15 +50,17 @@ export default function Form(props) {
             type="text"
             placeholder="Enter Student Name"
             onChange={inputName}
-          />
+            data-testid="student-name-input"
+            />
+        <section className="appointment__validation">{error}</section>
         </form>
-        <InterviewerList interviewers={interviewers} interviewer={interviewer} setInterviewer={handleInterviewer} />
+        <InterviewerList interviewers={interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
           {/* onSave function required to save name and interviewer selected */}
-          <Button confirm onClick={() => onSave(name, interviewer, creating)}>Save</Button>
+          <Button confirm onClick={() => validate()}>Save</Button>
         </section>
       </section>
     </main>
